30b4ed2f31d86bb39591b76e3186dd2141f7cec0
Marcin Kapelan <marcinkape@gmail.com>
2018-10-15 22:49:03 +0200

    Delete tutorial examples

--- src/main/java/com/paw/bettertrello/repositories/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.paw.bettertrello.repositories;
-
-import com.paw.bettertrello.models.Contact;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ContactRepository extends CrudRepository<Contact, String> {
-    @Override
-    void delete(Contact deleted);
-}
