2bff9353cbf18c142191d41a9d01a67dd7f99722
Marcin Kapelan <marcinkape@gmail.com>
2018-10-21 16:55:08 +0200

    Make findAll query of BoardRepository not return cardLists for huge speedup

--- src/main/java/com/paw/bettertrello/repositories/BoardRepository.java
+++ src/main/java/com/paw/bettertrello/repositories/BoardRepository.java
@@ -2,8 +2,14 @@ package com.paw.bettertrello.repositories;
 
 import com.paw.bettertrello.models.Board;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface BoardRepository extends MongoRepository<Board, String> {
     @Override
     void delete(Board deleted);
+    @Query(value = "{}", fields = "{ 'cardLists' : 0 }")
+    @Override
+    List<Board> findAll();
 }
