d775a9cb74c7e240de4b961ff6b1133928d20b34
Marcin Kapelan <marcinkape@gmail.com>
2018-10-21 02:54:17 +0200

    Add basic ETag filter

--- src/main/java/com/paw/bettertrello/BettertrelloApplication.java
+++ src/main/java/com/paw/bettertrello/BettertrelloApplication.java
@@ -2,9 +2,19 @@ package com.paw.bettertrello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import javax.servlet.Filter;
+
 
 @SpringBootApplication
 public class BettertrelloApplication {
+
+	@Bean
+	public Filter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BettertrelloApplication.class, args);
