fcff48ef01f8d0496cafd69d100d432916c17e70
Marcin Kapelan <marcinkape@gmail.com>
2018-11-05 17:14:49 +0100

    Add annotation enabling server to authenticate requests via oauth

--- src/main/java/com/paw/bettertrello/BettertrelloApplication.java
+++ src/main/java/com/paw/bettertrello/BettertrelloApplication.java
@@ -3,12 +3,14 @@ package com.paw.bettertrello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 import javax.servlet.Filter;
 
 
 @SpringBootApplication
+@EnableResourceServer
 public class BettertrelloApplication {
 
 	@Bean
