4b0009b242b626246af70c902e774f656535c9dd
Marcin Kapelan <marcinkape@gmail.com>
2018-11-06 21:35:31 +0100

    Change object type returned by UserRepository to Optional for consistency

--- src/main/java/com/paw/bettertrello/repositories/UserRepository.java
+++ src/main/java/com/paw/bettertrello/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.paw.bettertrello.repositories;
 import com.paw.bettertrello.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/com/paw/bettertrello/service/UserService.java
+++ src/main/java/com/paw/bettertrello/service/UserService.java
@@ -1,11 +1,14 @@
 package com.paw.bettertrello.service;
 
+import com.paw.bettertrello.models.User;
 import com.paw.bettertrello.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service("userDetailsService")
 public class UserService implements UserDetailsService {
@@ -15,6 +18,12 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username);
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+        if (optionalUser.isPresent()) {
+            return optionalUser.get();
+        }
+        else {
+            throw new UsernameNotFoundException(username + " not found");
+        }
     }
 }
