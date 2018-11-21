6f58774663f286260972137a64f40e3b686858a8
Marcin Kapelan <marcinkape@gmail.com>
2018-11-21 15:36:15 +0100

    Add authorization checks to get/post/put endpoints of CardListController

--- src/main/java/com/paw/bettertrello/controllers/CardListController.java
+++ src/main/java/com/paw/bettertrello/controllers/CardListController.java
@@ -4,6 +4,7 @@ package com.paw.bettertrello.controllers;
 import com.paw.bettertrello.models.Board;
 import com.paw.bettertrello.models.Card;
 import com.paw.bettertrello.models.CardList;
+import com.paw.bettertrello.repositories.BoardRepository;
 import com.paw.bettertrello.repositories.CardListRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -22,44 +24,64 @@ public class CardListController {
 
     @Autowired
     CardListRepository cardListRepository;
-
-
+    @Autowired
+    BoardRepository boardRepository;
 
     @ApiOperation(value = "Search a List with an ID",response = Optional.class)
     @RequestMapping(method=RequestMethod.GET, value="/lists/{id}")
-    public Optional<CardList> getList(@PathVariable String id) {
+    public ResponseEntity<?> getList(@PathVariable String id, Principal principal) {
+        String username = principal.getName();
         Optional<CardList> optionalCardList = cardListRepository.findById(id);
-        return optionalCardList;
+        if (optionalCardList.isPresent()) {
+            CardList cardList = optionalCardList.get();
+            return checkAuthorization(username, cardList, OkStatusBodyContent.CARDLIST);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @ApiOperation(value = "Search a list with an ID, a get's cards from there",response = Optional.class)
     @RequestMapping(method=RequestMethod.GET, value="/lists/{id}/cards")
-    public Iterable<Card> getCardsFromList(@PathVariable String id) {
+    public ResponseEntity<?> getCardsFromList(@PathVariable String id, Principal principal) {
+        String username = principal.getName();
         Optional<CardList> optionalCardList = cardListRepository.findById(id);
-        return optionalCardList.get().getCards();
+        if (optionalCardList.isPresent()) {
+            CardList cardList = optionalCardList.get();
+            return checkAuthorization(username, cardList, OkStatusBodyContent.CARDS);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @ApiOperation(value = "Add a Card to List",response = Board.class)
     @RequestMapping(method=RequestMethod.POST, value="/lists/{id}/cards")
-    public CardList postCardToList(@RequestBody Card card, @PathVariable String id) {
+    public ResponseEntity<?> postCardToList(@RequestBody Card card, @PathVariable String id, Principal principal) {
+        String username = principal.getName();
         Optional<CardList> optionalCardList = cardListRepository.findById(id);
-        if(optionalCardList.isPresent()) {
+        if (optionalCardList.isPresent()) {
             CardList cardList = optionalCardList.get();
-            if (cardList.getCards() == null) {
-                cardList.setCards(new ArrayList<Card>());
+            ResponseEntity<?> authorizationCheckResult = checkAuthorization(username, cardList, OkStatusBodyContent.EMPTY);
+            if (authorizationCheckResult.getStatusCode() != HttpStatus.OK) {
+                return authorizationCheckResult;
             }
+            if (cardList.getCards() == null) {
+                cardList.setCards(new ArrayList<>());
+            }
+            card.setParentBoardId(cardList.getParentBoardId());
             cardList.getCards().add(card);
-            return cardListRepository.save(cardList);
+            return new ResponseEntity<>(cardListRepository.save(cardList), HttpStatus.CREATED);
         }
         else {
-            return null;
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @ApiOperation(value = "Update a list")
     @RequestMapping(method=RequestMethod.PUT, value="/lists/{id}")
-    public ResponseEntity<?> updateList(@PathVariable String id, @RequestBody CardList cardList) {
-
+    public ResponseEntity<?> updateList(@PathVariable String id, @RequestBody CardList cardList, Principal principal) {
+        String username = principal.getName();
         Optional<CardList> optionalCardList;
 
         if (!(cardList.getId() == null || cardList.getId().isEmpty())) {
@@ -72,9 +94,14 @@ public class CardListController {
 
         if (optionalCardList.isPresent()) {
             CardList foundCardList = optionalCardList.get();
-            if(foundCardList.equals(cardList)) {
-                return new ResponseEntity<>(null, HttpStatus.FOUND);
-            }else {
+            ResponseEntity<?> authorizationCheckResult = checkAuthorization(username, foundCardList, OkStatusBodyContent.EMPTY);
+            if (authorizationCheckResult.getStatusCode() != HttpStatus.OK) {
+                return authorizationCheckResult;
+            }
+            if (foundCardList.equals(cardList)) {
+                return new ResponseEntity<>(HttpStatus.FOUND);
+            }
+            else {
                 return new ResponseEntity<>(cardListRepository.save(cardList), HttpStatus.OK);
             }
         }
@@ -94,4 +121,35 @@ public class CardListController {
         return "";
     }
 
+    private enum OkStatusBodyContent {
+        EMPTY,
+        CARDLIST,
+        CARDS
+    }
+
+    private ResponseEntity<?> checkAuthorization(String username, CardList cardList, OkStatusBodyContent bodyContent) {
+        if (cardList.getParentBoardId() == null || cardList.getParentBoardId().isEmpty()) {
+            return new ResponseEntity<>("List does not contain parent board ID", HttpStatus.BAD_REQUEST);
+        }
+        Optional<Board> optionalBoard = boardRepository.findById(cardList.getParentBoardId());
+        if (optionalBoard.isPresent()) {
+            Board board = optionalBoard.get();
+            if (board.getOwnerUsernames().contains(username)) {
+                switch (bodyContent) {
+                    case EMPTY:
+                        return new ResponseEntity<>(HttpStatus.OK);
+                    case CARDLIST:
+                        return new ResponseEntity<>(cardList, HttpStatus.OK);
+                    case CARDS:
+                        return new ResponseEntity<>(cardList.getCards(), HttpStatus.OK);
+                    default:
+                        throw new IllegalArgumentException();
+                }
+            }
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        else {
+            return new ResponseEntity<>("Parent board not found", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
