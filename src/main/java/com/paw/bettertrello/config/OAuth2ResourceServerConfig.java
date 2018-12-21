1f0e247357df65bb3a09a4a38501fec11507a67f
Marcin Kapelan <marcinkape@gmail.com>
2018-12-21 03:26:41 +0100

    Whitelist Swagger urls in OAuth config

--- src/main/java/com/paw/bettertrello/config/OAuth2ResourceServerConfig.java
+++ src/main/java/com/paw/bettertrello/config/OAuth2ResourceServerConfig.java
@@ -10,6 +10,16 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    private static final String[] SWAGGER_ENDPOINTS = {
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+    };
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
@@ -17,6 +27,7 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
                 .authorizeRequests()
                 .antMatchers("/users").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll()
+                .antMatchers(SWAGGER_ENDPOINTS).permitAll()
                 .anyRequest().authenticated();
     }
 }
